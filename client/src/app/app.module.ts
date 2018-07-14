1361b2185c6fcd45d224fce6b5c67f6fea24979f
Ivan Kozlov <ikozlov@student.42.us.org>
2018-07-13 19:17:39 -0700

    Upd

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { IndexComponent } from './views/index/index.component';
+import { FooterComponent } from './views/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

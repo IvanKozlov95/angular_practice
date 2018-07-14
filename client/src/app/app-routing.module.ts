5ca367f89e1568514e5fda4a7d4685a6ac1d68a2
Ivan Kozlov <ikozlov@student.42.us.org>
2018-07-13 19:17:08 -0700

    Added routing module

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { IndexComponent } from './views/index/index.component';
+
+const routes: Routes = [
+	{ path: '', component: IndexComponent }
+]
+
+@NgModule({
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
+})
+export class AppRoutingModule { }

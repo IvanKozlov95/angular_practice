36e472f3270fb47945475fe4dfd53550a16454d0
Ivan Kozlov <ikozlov@student.42.us.org>
2018-07-16 02:35:29 -0700

    Added navbar and header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
@@ -7,18 +7,23 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IndexComponent } from './views/index/index.component';
 import { FooterComponent } from './views/layout/footer/footer.component';
+import { HeaderComponent } from './views/layout/header/header.component';
+import { NavbarComponent } from './views/layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
